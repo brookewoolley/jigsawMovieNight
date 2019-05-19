85a6f0d882aa96d12f74d3fb768da3fbd6fff131
Brooke Woolley <brooke.woolley@gmail.com>
2019-05-19 16:44:42 +0100

    :construction: successful call to external api, console log the results

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+const API = require("./config.json");
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.componentDidMount(this.state.value);
+    event.preventDefault();
+  }
+
+  componentDidMount(value) {
+    fetch(`${API.baseUrl}search/movie?api_key=${API.apiKey}&query=${value}`)
+      .then(response => response.json())
+      .then(response => console.log(response));
+    // data => this.setState({ movies: data.movieRequest })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Search for a film:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
